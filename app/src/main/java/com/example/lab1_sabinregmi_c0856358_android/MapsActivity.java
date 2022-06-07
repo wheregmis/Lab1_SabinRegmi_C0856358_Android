b5cc6667849bd41babc7bab5dbdd71359073f1a3
Sabin Regmi <xawbeenregmi@gmail.com>
2022-06-07 18:49:16 -0400

    Drawing Polygon From Points ABCD
    -- drawing polygon from points ABCD

--- app/src/main/java/com/example/lab1_sabinregmi_c0856358_android/MapsActivity.java
+++ app/src/main/java/com/example/lab1_sabinregmi_c0856358_android/MapsActivity.java
@@ -10,10 +10,13 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.Point;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -28,9 +31,15 @@ import com.example.lab1_sabinregmi_c0856358_android.databinding.ActivityMapsBind
 import com.google.android.gms.maps.model.Polygon;
 import com.google.android.gms.maps.model.PolygonOptions;
 import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -39,8 +48,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private static final int REQUEST_CODE = 1;
     private Marker homeMarker;
-    
+
     List<Marker> markers = new ArrayList();
+
+    // for drawing polygon
+    Polygon shape;
+    private static final int POLYGON_SIDES = 4;
 
     // location with location manager and listener
     LocationManager locationManager;
@@ -106,10 +119,115 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 setMarker(latLng);
             }
             private void setMarker(LatLng latLng) {
-                MarkerOptions options = new MarkerOptions().position(latLng)
-                        .title("Your destination");
 
-                markers.add(mMap.addMarker(options));
+//                String json_str = "{\"AD\":\"Europe\",\"AE\":\"Asia\",\"AF\":\"Asia\",\"AG\":\"North America\",\"AI\":\"North America\",\"AL\":\"Europe\",\"AM\":\"Asia\",\"AN\":\"North America\",\"AO\":\"Africa\",\"AQ\":\"Antarctica\",\"AR\":\"South America\",\"AS\":\"Australia\",\"AT\":\"Europe\",\"AU\":\"Australia\",\"AW\":\"North America\",\"AZ\":\"Asia\",\"BA\":\"Europe\",\"BB\":\"North America\",\"BD\":\"Asia\",\"BE\":\"Europe\",\"BF\":\"Africa\",\"BG\":\"Europe\",\"BH\":\"Asia\",\"BI\":\"Africa\",\"BJ\":\"Africa\",\"BM\":\"North America\",\"BN\":\"Asia\",\"BO\":\"South America\",\"BR\":\"South America\",\"BS\":\"North America\",\"BT\":\"Asia\",\"BW\":\"Africa\",\"BY\":\"Europe\",\"BZ\":\"North America\",\"CA\":\"North America\",\"CC\":\"Asia\",\"CD\":\"Africa\",\"CF\":\"Africa\",\"CG\":\"Africa\",\"CH\":\"Europe\",\"CI\":\"Africa\",\"CK\":\"Australia\",\"CL\":\"South America\",\"CM\":\"Africa\",\"CN\":\"Asia\",\"CO\":\"South America\",\"CR\":\"North America\",\"CU\":\"North America\",\"CV\":\"Africa\",\"CX\":\"Asia\",\"CY\":\"Asia\",\"CZ\":\"Europe\",\"DE\":\"Europe\",\"DJ\":\"Africa\",\"DK\":\"Europe\",\"DM\":\"North America\",\"DO\":\"North America\",\"DZ\":\"Africa\",\"EC\":\"South America\",\"EE\":\"Europe\",\"EG\":\"Africa\",\"EH\":\"Africa\",\"ER\":\"Africa\",\"ES\":\"Europe\",\"ET\":\"Africa\",\"FI\":\"Europe\",\"FJ\":\"Australia\",\"FK\":\"South America\",\"FM\":\"Australia\",\"FO\":\"Europe\",\"FR\":\"Europe\",\"GA\":\"Africa\",\"GB\":\"Europe\",\"GD\":\"North America\",\"GE\":\"Asia\",\"GF\":\"South America\",\"GG\":\"Europe\",\"GH\":\"Africa\",\"GI\":\"Europe\",\"GL\":\"North America\",\"GM\":\"Africa\",\"GN\":\"Africa\",\"GP\":\"North America\",\"GQ\":\"Africa\",\"GR\":\"Europe\",\"GS\":\"Antarctica\",\"GT\":\"North America\",\"GU\":\"Australia\",\"GW\":\"Africa\",\"GY\":\"South America\",\"HK\":\"Asia\",\"HN\":\"North America\",\"HR\":\"Europe\",\"HT\":\"North America\",\"HU\":\"Europe\",\"ID\":\"Asia\",\"IE\":\"Europe\",\"IL\":\"Asia\",\"IM\":\"Europe\",\"IN\":\"Asia\",\"IO\":\"Asia\",\"IQ\":\"Asia\",\"IR\":\"Asia\",\"IS\":\"Europe\",\"IT\":\"Europe\",\"JE\":\"Europe\",\"JM\":\"North America\",\"JO\":\"Asia\",\"JP\":\"Asia\",\"KE\":\"Africa\",\"KG\":\"Asia\",\"KH\":\"Asia\",\"KI\":\"Australia\",\"KM\":\"Africa\",\"KN\":\"North America\",\"KP\":\"Asia\",\"KR\":\"Asia\",\"KW\":\"Asia\",\"KY\":\"North America\",\"KZ\":\"Asia\",\"LA\":\"Asia\",\"LB\":\"Asia\",\"LC\":\"North America\",\"LI\":\"Europe\",\"LK\":\"Asia\",\"LR\":\"Africa\",\"LS\":\"Africa\",\"LT\":\"Europe\",\"LU\":\"Europe\",\"LV\":\"Europe\",\"LY\":\"Africa\",\"MA\":\"Africa\",\"MC\":\"Europe\",\"MD\":\"Europe\",\"ME\":\"Europe\",\"MG\":\"Africa\",\"MH\":\"Australia\",\"MK\":\"Europe\",\"ML\":\"Africa\",\"MM\":\"Asia\",\"MN\":\"Asia\",\"MO\":\"Asia\",\"MP\":\"Australia\",\"MQ\":\"North America\",\"MR\":\"Africa\",\"MS\":\"North America\",\"MT\":\"Europe\",\"MU\":\"Africa\",\"MV\":\"Asia\",\"MW\":\"Africa\",\"MX\":\"North America\",\"MY\":\"Asia\",\"MZ\":\"Africa\",\"NA\":\"Africa\",\"NC\":\"Australia\",\"NE\":\"Africa\",\"NF\":\"Australia\",\"NG\":\"Africa\",\"NI\":\"North America\",\"NL\":\"Europe\",\"NO\":\"Europe\",\"NP\":\"Asia\",\"NR\":\"Australia\",\"NU\":\"Australia\",\"NZ\":\"Australia\",\"OM\":\"Asia\",\"PA\":\"North America\",\"PE\":\"South America\",\"PF\":\"Australia\",\"PG\":\"Australia\",\"PH\":\"Asia\",\"PK\":\"Asia\",\"PL\":\"Europe\",\"PM\":\"North America\",\"PN\":\"Australia\",\"PR\":\"North America\",\"PS\":\"Asia\",\"PT\":\"Europe\",\"PW\":\"Australia\",\"PY\":\"South America\",\"QA\":\"Asia\",\"RE\":\"Africa\",\"RO\":\"Europe\",\"RS\":\"Europe\",\"RU\":\"Europe\",\"RW\":\"Africa\",\"SA\":\"Asia\",\"SB\":\"Australia\",\"SC\":\"Africa\",\"SD\":\"Africa\",\"SE\":\"Europe\",\"SG\":\"Asia\",\"SH\":\"Africa\",\"SI\":\"Europe\",\"SJ\":\"Europe\",\"SK\":\"Europe\",\"SL\":\"Africa\",\"SM\":\"Europe\",\"SN\":\"Africa\",\"SO\":\"Africa\",\"SR\":\"South America\",\"ST\":\"Africa\",\"SV\":\"North America\",\"SY\":\"Asia\",\"SZ\":\"Africa\",\"TC\":\"North America\",\"TD\":\"Africa\",\"TF\":\"Antarctica\",\"TG\":\"Africa\",\"TH\":\"Asia\",\"TJ\":\"Asia\",\"TK\":\"Australia\",\"TM\":\"Asia\",\"TN\":\"Africa\",\"TO\":\"Australia\",\"TR\":\"Asia\",\"TT\":\"North America\",\"TV\":\"Australia\",\"TW\":\"Asia\",\"TZ\":\"Africa\",\"UA\":\"Europe\",\"UG\":\"Africa\",\"US\":\"North America\",\"UY\":\"South America\",\"UZ\":\"Asia\",\"VC\":\"North America\",\"VE\":\"South America\",\"VG\":\"North America\",\"VI\":\"North America\",\"VN\":\"Asia\",\"VU\":\"Australia\",\"WF\":\"Australia\",\"WS\":\"Australia\",\"YE\":\"Asia\",\"YT\":\"Africa\",\"ZA\":\"Africa\",\"ZM\":\"Africa\",\"ZW\":\"Africa\"}";
+//
+//                try {
+//
+//                    JSONObject jsonObject = new JSONObject(json_str);
+//
+//                    Geocoder geocoder = new Geocoder(MapsActivity.this, Locale.ENGLISH);
+//
+//                    List<Address> addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+//
+//                    if (addresses.size() > 0) {
+//
+//                        Address fetchedAddress = addresses.get(0);
+//
+//                        // getCountryCode from Address
+//                        String countryCode = fetchedAddress.getCountryCode();
+//
+//                        // get continentName here
+//                        String continentName = jsonObject.getString(countryCode);
+//
+//                        if (continentName == "North America"){
+//
+//
+//                        }
+//                        Log.d("Contient Test", "setMarker: "+continentName);
+//                    }else{
+//                        MarkerOptions options = new MarkerOptions().position(latLng)
+//                                .title("F");
+//
+//                        // check if there are already the same number of markers, we clear the map.
+//                        markers.add(mMap.addMarker(options));
+//
+//                    }
+//
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+
+                if (markers.size() == 0){
+                    MarkerOptions options = new MarkerOptions().position(latLng)
+                            .title("A");
+
+                    // check if there are already the same number of markers, we clear the map.
+                    markers.add(mMap.addMarker(options));
+                }else if (markers.size() == 1){
+                    MarkerOptions options = new MarkerOptions().position(latLng)
+                            .title("B");
+
+                    // check if there are already the same number of markers, we clear the map.
+                    markers.add(mMap.addMarker(options));
+                }else if (markers.size() == 2){
+                    MarkerOptions options = new MarkerOptions().position(latLng)
+                            .title("C");
+
+                    // check if there are already the same number of markers, we clear the map.
+                    markers.add(mMap.addMarker(options));
+                }else if (markers.size() == 3){
+                    MarkerOptions options = new MarkerOptions().position(latLng)
+                            .title("D");
+
+                    // check if there are already the same number of markers, we clear the map.
+                    markers.add(mMap.addMarker(options));
+                }else{
+                    MarkerOptions options = new MarkerOptions().position(latLng)
+                            .title("P");
+
+                    // check if there are already the same number of markers, we clear the map.
+                    markers.add(mMap.addMarker(options));
+                }
+
+                if (markers.size() == POLYGON_SIDES)
+                    drawShape();
+
+            }
+
+            private void drawShape() {
+                PolygonOptions options = new PolygonOptions()
+                        .fillColor(0x5900FF00)
+                        // 0xFF00FF00 is green and 59 instead of FF is 35% transparency
+                        .strokeColor(Color.RED)
+                        .strokeWidth(5);
+
+                ArrayList<LatLng> sourcePoints = new ArrayList<>();
+
+                for (int i=0; i<POLYGON_SIDES; i++) {
+                    sourcePoints.add(markers.get(i).getPosition());
+
+                }
+                Projection projection = mMap.getProjection();
+                ArrayList<Point> screenPoints = new ArrayList<>(sourcePoints.size());
+                for (LatLng location : sourcePoints) {
+                    Point p = projection.toScreenLocation(location);
+                    screenPoints.add(p);
+                }
+
+                ArrayList<Point> convexHullPoints = convexHull(screenPoints);
+                ArrayList<LatLng> convexHullLocationPoints = new ArrayList(convexHullPoints.size());
+                for (Point screenPoint : convexHullPoints) {
+                    LatLng location = projection.fromScreenLocation(screenPoint);
+                    convexHullLocationPoints.add(location);
+                }
+
+                for (LatLng latLng : convexHullLocationPoints) {
+                    options.add(latLng);
+                }
+                shape = mMap.addPolygon(options);
             }
         });
 
@@ -154,5 +272,44 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private boolean hasLocationPermission() {
         return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+
+    private boolean CCW(Point p, Point q, Point r) {
+        return (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y) > 0;
+    }
+
+    public ArrayList<Point> convexHull(ArrayList<Point> points)
+    {
+        int n = points.size();
+        if (n <= 3) return points;
+
+        ArrayList<Integer> next = new ArrayList<>();
+
+        // find the leftmost point
+        int leftMost = 0;
+        for (int i = 1; i < n; i++)
+            if (points.get(i).x < points.get(leftMost).x)
+                leftMost = i;
+        int p = leftMost, q;
+        next.add(p);
+
+        // iterate till p becomes leftMost
+        do {
+            q = (p + 1) % n;
+            for (int i = 0; i < n; i++)
+                if (CCW(points.get(p), points.get(i), points.get(q)))
+                    q = i;
+            next.add(q);
+            p = q;
+        } while (p != leftMost);
+
+        ArrayList<Point> convexHullPoints = new ArrayList();
+        for (int i = 0; i < next.size() - 1; i++) {
+            int ix = next.get(i);
+            convexHullPoints.add(points.get(ix));
+        }
+
+        return convexHullPoints;
     }
 }
